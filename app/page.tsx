7823443df3a5d4dd3945d71711f11ac0410a6dcb
Moshe Asulin <xp16oc@gmail.com>
2024-09-11 12:39:30 +0300

    new about route

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/about"
             rel="noopener noreferrer"
           >
             <Image
@@ -52,7 +51,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/about"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -67,7 +66,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/about"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -82,7 +81,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/about"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <section className="py-20">
+      <h1 className="text-4xl font-bold mb-4">About</h1>
+      <p>
+        This is a simple Next.js application that demonstrates how to use
+        Tailwind CSS and PostCSS to style your application.
+      </p>
+    </section>
+  );
+}
