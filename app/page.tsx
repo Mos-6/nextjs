ce916972934da559530fd085f767027640d10efa
Moshe Asulin <xp16oc@gmail.com>
2024-09-11 12:40:29 +0300

    switch a with Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -24,7 +25,7 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/about"
             rel="noopener noreferrer"
@@ -37,19 +38,21 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
+          </Link>
+          <Link
+          
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+          <Link
+        
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/about"
           target="_blank"
@@ -63,8 +66,9 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
-        <a
+        </>
+          <Link
+        
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/about"
           target="_blank"
@@ -78,8 +82,9 @@ export default function Home() {
             height={16}
           />
           Examples
-        </a>
-        <a
+        </>
+          <Link
+        
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/about"
           target="_blank"
@@ -93,7 +98,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </>
       </footer>
     </div>
   );
